5a2cdcbfeb750ee1f4f43c8ce37be7acd1e955fc
souzabg <bruno.garbuio.souza@gmail.com>
2018-12-31 12:18:02 -0500

    fff

--- Code/robot.py
+++ Code/robot.py
@@ -5,28 +5,36 @@ import time
 
 
 def initialize():
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BOARD)
     
 class motor:
     pin = {1,1,1,1}
-    def initialize():
-        GPIO.setup(self.pin[0].GPIO.OUT)
-        GPIO.setup(self.pin[1].GPIO.OUT)
-        GPIO.setup(self.pin[2].GPIO.OUT)
-        GPIO.setup(self.pin[3].GPIO.OUT)
-        
-        GPIO.output(self.pin[0].GPIO.LOW)
-        GPIO.output(self.pin[1].GPIO.LOW)
-        GPIO.output(self.pin[2].GPIO.LOW)
-        GPIO.output(self.pin[3].GPIO.LOW)
-        
-        
-        
-        
-        
+    def initialize(self):
+        for p in self.pin:
+            GPIO.setup(p,GPIO.OUT)    
+            GPIO.output(p,GPIO.LOW)
+               
         
 def main():
     print("Robot Initialize")
-    
+    GPIO.setwarnings(False)
+    initialize()
+    motorbl = motor()
+    motorfl = motor()
+    motorbr = motor()
+    motorfr = motor()
+    motorbl.pin = {37,35,33,31}
+    motorfl.pin = {40,38,36,32}
+    motorbr.pin = {7,11,13,15}
+    motorfr.pin = {8,10,12,16}
+
+    motorbl.initialize()
+    motorbr.initialize()
+    motorfl.initialize()
+    motorfr.initialize()
+
+
+
+
 if __name__ =="__main__":
     main()
