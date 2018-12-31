b0d912ff3d5d5f5dbe6907d5d7f0a850a1b3907c
souzabg <bruno.garbuio.souza@gmail.com>
2018-12-31 11:24:57 -0500

    Lebron James

--- Code/robot.py
+++ Code/robot.py
@@ -2,4 +2,20 @@
 import RPI.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+
+
+def initialize()
+    GPIO.setmode(GPIO.BCM)
+    
+class motor:
+    pin[]={1,1,1,1}
+    def initialize()
+        GPIO.setup(self.pin[0].GPIO.OUT)
+        GPIO.setup(self.pin[1].GPIO.OUT)
+        GPIO.setup(self.pin[2].GPIO.OUT)
+        GPIO.setup(self.pin[3].GPIO.OUT)
+        
+        GPIO.output(self.pin[0].GPIO.LOW)
+        GPIO.output(self.pin[1].GPIO.LOW)
+        GPIO.output(self.pin[2].GPIO.LOW)
+        GPIO.output(self.pin[3].GPIO.LOW)
